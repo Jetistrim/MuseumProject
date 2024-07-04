b02de6933f33eb4fc57357f150f2ca4af79980ab
Paulo Gabriel Lima De Sousa <paulo0882@DIGITALCOLLEGE.LOCAL>
2024-07-04 10:06:45 -0300

    minor updates Home

--- src/pages/SitePages/Home/index.jsx
+++ src/pages/SitePages/Home/index.jsx
@@ -4,15 +4,17 @@ import HomeTexts from "../../../components/homeTexts";
 const Home = () => {
     const DivContainer = styled.div`
         margin-top: 200px;
-        #caixa-flutuante {
+        & #caixa-flutuante {
             
         }
     `;
 
     return (
         <DivContainer className="flex flex-column">
-            <div id="caixa-flutuante" className="flex align-self-end bg-gray-500 w-23rem p-4 mr-8 border-round">
-            </div>
+            <div
+                id="caixa-flutuante"
+                className="flex align-self-end bg-gray-500 w-23rem h-10rem p-4 mr-8 border-round translate-y-100"
+            ></div>
             <div className="flex justify-content-center flex-column align-items-center bg-gray-300 px-3 p-3">
                 <HomeTexts />
                 <div className="w-max flex align-items-center justify-content-center m-2">
